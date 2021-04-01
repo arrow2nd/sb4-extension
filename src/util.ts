6891b19d4918acd1e817118ba75eb01abf397d7c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-04-01 15:04:15 +0900

    remove: util

--- src/util.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode'
-
-export function createMarkdown(desc: string, shouldShowHr: boolean) {
-  const message = new vscode.MarkdownString()
-
-  message.appendCodeblock('test')
-
-  return message
-}
