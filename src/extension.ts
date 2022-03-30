97b725cc7d406ef6eca81e8e6ffcfc3b5183f4e4
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-30 20:42:35 +0900

    feat: web拡張に対応

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,8 @@ export function activate(context: vscode.ExtensionContext) {
   // 全体をスキャン
   scan.update()
 
+  console.log('Hello!')
+
   // 画面が切り替わった
   const handleChangeActiveEditor = vscode.window.onDidChangeActiveTextEditor(
     (event) => {
